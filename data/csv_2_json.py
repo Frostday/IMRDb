72bb0ca3830fa9b1b0e4e3379dbbbdbb12470d41
FrostDayee <dhruvgarg01@gmail.com>
2020-09-20 20:11:23 +0530

    added json

--- data/csv_2_json.py
+++ data/csv_2_json.py
@@ -1,25 +1,24 @@
-import csv 
-import json 
-  
-def make_json(csvFilePath, jsonFilePath): 
+import pandas as pd
 
-    data = {} 
-      
-    # Open a csv reader called DictReader 
-    with open(csvFilePath, encoding='utf-8') as csvf: 
-        csvReader = csv.DictReader(csvf) 
+df = pd.read_csv("data/combined.csv")
 
-        for rows in csvReader: 
-            key = rows['movieId'] 
-            data[key] = rows 
-  
-    # Open a json writer, and use the json.dumps()  
-    # function to dump data 
-    with open(jsonFilePath, 'w', encoding='utf-8') as jsonf: 
-        jsonf.write(json.dumps(data, indent=4)) 
-          
+with open("data/database.json", "w") as f:
+    f.write('[\n')
+    for index, row in df.iterrows():
+        f.write('\t{\n')
 
-csvFilePath = r'combined.csv'
-jsonFilePath = r'database.json'
-  
-make_json(csvFilePath, jsonFilePath)
+        f.write('\t\t"model": "mainpage.movie",\n')
+        f.write('\t\t"pk": "{}",\n'.format(index))
+        f.write('\t\t"fields": {\n')
+        
+        name = row['title']
+        i = row['movieId']
+        url = row['image_url']
+        
+        f.write('\t\t\t"name": "{}",\n'.format(name))
+        f.write('\t\t\t"id": "{}",\n'.format(i))
+        f.write('\t\t\t"image_url": "{}"\n'.format(url))
+
+        f.write('\t\t}\n')
+        f.write('\t},\n')
+    f.write(']')
